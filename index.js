1c0eb94c03b0a3de327a0bda7cba4ab45557106f
Christopher Grady <grittygrady@gmail.com>
2020-06-22 09:24:24 -0700

    fixed error functionality

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ function displayDog(responseJson) {
 
 function getBreedPic() {
   let selectedBreed = $("#breed").val();
-  let breed = selectedBreed.toLowerCase().split(" ").join("-");
+  let breed = selectedBreed.toLowerCase().split(" ").reverse().join("-");
   console.log(breed);
   fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(response => response.json())
-    .then(responseJson => displayDog(responseJson))
-    .catch(error => alert(`No breed by that name found. Try again!`));
-}
+    .then(responseJson => {
+      if (responseJson.status === "success") {
+        displayDog(responseJson);
+      } else {
+        alert('No dog by that breed found, try again! Try using only the breed, not the sub-breed.')
+      }
+    })
+    .catch(error => alert(`Something went wrong, please try again.`));
+  }
+
 
 function formListener() {
   $("form").on("submit", function(event){
